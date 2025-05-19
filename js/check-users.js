66212186de71bfdc1500f40980234516447bf528
TahaUgan <taha.ugan55@gmail.com>
2025-05-19 15:09:07 +0300

    Delete js directory

--- js/check-users.js
+++
@@ -1,8 +0,0 @@
-// Get the stored data from localStorage
-const storedData = localStorage.getItem('hotelData');
-if (storedData) {
-    const data = JSON.parse(storedData);
-    console.log('Stored Users:', data.users);
-} else {
-    console.log('No data found in localStorage');
-} 
